346ab1e663572b7b492b65b5d76bce9917ae5ea0
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-08-20 09:27:39 -0300

    Notes finished + Note DevKeys

--- s-be/routes/notes.js
+++ s-be/routes/notes.js
@@ -18,9 +18,104 @@ router.post('/newNote', authenticateToken, async (req, res) => {
             }
         })
 
-        return res.status(201).json({ message: 'Added note', note: noted })
+        return res.status(201).json({ message: 'Nota adicionada', note: noted })
     } catch (err) {
-        return res.status(400).json({ message: 'Error' })
+        return res.status(400).json({ message: err })
+    }
+})
+
+router.get('/getNotes', authenticateToken, async (req, res) => {
+    try {
+        const uId = req.user.id
+        let notes = await prisma.notes.findMany({ where: {User_id: uId}})
+        notes.forEach((note) => {
+            if (note.isDeleted) {
+                let index = notes.indexOf(note)
+                if (index > -1) {
+                    notes.splice(index, 1)
+                }
+            }
+        })
+
+        return res.status(200).json({ notes: notes})
+    } catch (err) {
+        return res.status(400).json({ message: err})
+    }
+})
+
+router.put('/editNote/:id', async (req, res) => {
+    try {
+        const { notename, note } = req.body
+        let { id } = req.params
+        id = Number(id)
+
+        let noted = await prisma.notes.update({
+            where: { id },
+            data: {
+                notename,
+                note
+            }
+        })
+
+        return res.status(200).json({ message: 'Nota atualizada', note: noted })
+    } catch (err) {
+        return res.status(400).json({ message: err })
+    }
+})
+
+router.delete('/delNote/:id/:type', async (req, res) => {
+    try {
+        const { type } = req.params
+        let { id } = req.params
+        id = Number(id)
+
+        if (type.toLocaleLowerCase() == 'hard') {
+            await prisma.notes.delete({ where: { id } })
+        } else {
+            await prisma.notes.update({
+                where: { id },
+                data: {
+                    isDeleted: true
+                }
+            })
+        }
+
+        return res.sendStatus(204)
+    } catch (err) {
+        return res.status(400).json({ message: err })
+    }
+})
+
+router.get('/devkeys/getAll', async (req, res) => {
+    try {
+        const notes = await prisma.notes.findMany()
+
+        return res.status(200).json({ notes: notes})
+    } catch (err) {
+        return res.status(400).json({ message: err })
+    }
+})
+
+router.delete('/devkeys/undelete/:id', async (req, res) => {
+    try {
+        let { id } = req.params
+        id = Number(id)
+
+        let note = await prisma.notes.findUnique({ where: {id}})
+        if (!note.isDeleted) {
+            return res.status(202).json({ message: 'Não há necessidade de desdeletar uma nota que não está deletada'})
+        }
+
+        const unNote = await prisma.notes.update({
+            where: { id },
+            data: {
+                isDeleted: false
+            }
+        })
+
+        return res.status(201).json({ message: 'Nota desdeletada', note: unNote})
+    } catch (err) {
+        return res.status(400).json({ message: err })
     }
 })
 
