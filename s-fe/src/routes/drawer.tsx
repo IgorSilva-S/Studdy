ec622ffb98d8893b32f305cc0f24868c9c6d2686
CAMILLY VITORIA LAZZARO FERREIRA <CG3026311@car.ifsp>
2025-08-29 11:51:34 -0300

    Finish Página Inicial

--- s-fe/src/routes/drawer.tsx
+++ s-fe/src/routes/drawer.tsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 // Screens
 import Home from "../screens/Home";
@@ -9,34 +10,105 @@ import Notes from "../screens/Notes";
 import Todo from "../screens/Todo";
 import Timer from "../screens/Timer";
 
-const Drawer = createDrawerNavigator()
+const Drawer = createDrawerNavigator();
 
 function DrawRoutes() {
-    return (
-        <Drawer.Navigator screenOptions={{
-            drawerType: "back",
-            drawerStyle: {
-                backgroundColor: '#7fabc6'
-            },
-            swipeEdgeWidth: 80,
-            drawerActiveTintColor: '#f0f8ff',
-            drawerLabelStyle: {
-                color: '#deecf5'
-            }
-        }}>
-            <Drawer.Screen name="home" component={Home} options={{ drawerLabel: 'Início', title: '', headerStyle: { backgroundColor: '#deecf5', elevation: 0 }, drawerIcon: ({color, size}) => <Feather name="home" color={color} size={size}/> }} />
-            <Drawer.Screen name="account" component={Account} options={{ drawerLabel: 'Conta', title: '', headerStyle: { backgroundColor: '#deecf5', elevation: 0 }, drawerIcon: ({color, size}) => <Feather name="user" color={color} size={size}/> }} />
-            <Drawer.Screen name="todo" component={Todo} options={{ drawerLabel: 'Afazeres', title: '', headerStyle: { backgroundColor: '#deecf5', elevation: 0 }, drawerIcon: ({color, size}) => <Feather name="check-circle" color={color} size={size}/> }} />
-            <Drawer.Screen name="notes" component={Notes} options={{ drawerLabel: 'Anotações', title: '', headerStyle: { backgroundColor: '#deecf5', elevation: 0 }, drawerIcon: ({color, size}) => <FontAwesome name="sticky-note-o" color={color} size={size}/> }} />
-            <Drawer.Screen name="timer" component={Timer} options={{ drawerLabel: 'Temporizador', title: '', headerStyle: { backgroundColor: '#deecf5', elevation: 0 }, drawerIcon: ({color, size}) => <Ionicons name="timer-outline" color={color} size={size}/> }} />
-        </Drawer.Navigator>
-    )
+  return (
+    <Drawer.Navigator
+    screenOptions={({ navigation }) => ({
+      drawerType: "back",
+      drawerStyle: {
+        backgroundColor: "#7fabc6", 
+      },
+      swipeEdgeWidth: 80,
+      drawerActiveTintColor: "#f0f8ff",
+      drawerLabelStyle: {
+        color: "#7fabc6",
+        fontSize: 18,
+        fontWeight: "600",
+      },
+      headerStyle: {
+        backgroundColor: "#deecf5",
+        elevation: 0,
+      },
+      headerLeft: () => (
+        <TouchableOpacity
+          onPress={() => navigation.toggleDrawer()}
+          style={{
+            marginLeft: 15,
+            backgroundColor: "transparent", 
+            borderRadius: 50,
+            zIndex: 999, 
+          }}
+        >
+          <Feather name="menu" size={40} color="#7fabc6" />
+        </TouchableOpacity>
+      ),
+    })}
+  >  
+      <Drawer.Screen
+        name="home"
+        component={Home}
+        options={{
+          drawerLabel: "Início",
+          title: "",
+          drawerIcon: ({ color }) => (
+            <Feather name="home" color={color} size={28} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="account"
+        component={Account}
+        options={{
+          drawerLabel: "Conta",
+          title: "",
+          drawerIcon: ({ color }) => (
+            <Feather name="user" color={color} size={28} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="todo"
+        component={Todo}
+        options={{
+          drawerLabel: "Afazeres",
+          title: "",
+          drawerIcon: ({ color }) => (
+            <Feather name="check-circle" color={color} size={28} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="notes"
+        component={Notes}
+        options={{
+          drawerLabel: "Anotações",
+          title: "",
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="sticky-note-o" color={color} size={28} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="timer"
+        component={Timer}
+        options={{
+          drawerLabel: "Temporizador",
+          title: "",
+          drawerIcon: ({ color }) => (
+            <Ionicons name="timer-outline" color={color} size={28} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
 }
 
 export default function RootNav() {
-    return (
-        <NavigationContainer>
-            <DrawRoutes />
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <DrawRoutes />
+    </NavigationContainer>
+  );
 }
--- s-fe/src/screens/Home.tsx
+++ s-fe/src/screens/Home.tsx
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import "react-native-gesture-handler";
+import { Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/img/logo.png")}
+          style={styles.logo}
+        />
+      </View>
+
+      <Image
+        source={require("../../assets/img/buddy.png")}
+        style={styles.bear}
+      />
+
+      <Text style={styles.subtitle}>Studdy, o seu buddy{"\n"}dos estudos!</Text>
+
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button}>
+          <Feather name="clock" size={24} color="#3c5a6b" />
+          <Text style={styles.buttonText}> Cronômetro</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <FontAwesome name="check" size={24} color="#3c5a6b" />
+          <Text style={styles.buttonText}> Listas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Ionicons name="document" size={24} color="#3c5a6b" />
+          <Text style={styles.buttonText}> Notas</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +45,64 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#deecf5',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#deecf5",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 40,
   },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  menu: {
+    fontSize: 28,
+    color: "#3c5a6b",
+  },
+  logo: {
+    width: 380,
+    height: 55,
+    resizeMode: "contain",
+    position: "absolute",
+    top: -95,
+    left: 130,
+    alignSelf: "center",
+    zIndex: 999,
+  },
+  bear: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    marginVertical: 10,
+    marginTop: 40,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: "center",
+    color: "#3c5a6b",
+    marginBottom: 20,
+    marginTop: 30,
+  },
+  buttons: {
+    width: "80%",
+    marginTop: 10,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',  
+    backgroundColor: '#deecf5',
+    borderWidth: 2,
+    borderColor: '#8bb6cf',
+    borderRadius: 10,
+    padding: 12,
+    marginVertical: 6,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#3c5a6b',
+    marginLeft: 10,          
+  }  
 });
